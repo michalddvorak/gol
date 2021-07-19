8b4b4831aaa48cf7b9558f29da265226104c0994
Michal Dvořák <dvora125@fit.cvut.cz>
2021-07-19 23:55:23 +0200

    reworked bitmap

--- bitmap.cpp
+++ bitmap.cpp
@@ -2,16 +2,69 @@
 
 using namespace std::string_literals;
 
-std::vector<bool>& bitmap::operator [](size_t i)
+bitmap::bitmap(size_t rows, size_t cols) : m_rows(rows),
+										   m_cols(cols),
+										   m_interal_array(new bool* [rows]),
+										   m_actual_array(new bool[rows * cols])
 {
-	return m_map[i];
+	init_internal_array();
 }
-bitmap::bitmap(size_t rows, size_t cols) : m_map(rows, std::vector<bool>(cols)),
-										   m_rows(rows),
-										   m_cols(cols) { }
-const std::vector<bool>& bitmap::operator [](size_t i) const
+
+
+bitmap::bitmap(const bitmap& other) : m_rows(other.m_rows),
+									  m_cols(other.m_cols),
+									  m_interal_array(new bool* [m_rows]),
+									  m_actual_array(new bool[m_rows * m_cols])
 {
-	return m_map[i];
+	std::copy(other.m_actual_array, other.m_actual_array + m_rows * m_cols, m_actual_array);
+	init_internal_array();
+}
+
+bitmap::bitmap(bitmap&& other) noexcept: m_rows(other.m_rows),
+										 m_cols(other.m_cols),
+										 m_interal_array(std::exchange(other.m_interal_array, nullptr)),
+										 m_actual_array(std::exchange(other.m_actual_array, nullptr)) { }
+
+bitmap::~bitmap()
+{
+	delete[] m_actual_array;
+	delete[] m_interal_array;
+}
+
+
+bitmap& bitmap::operator =(bitmap other) noexcept
+{
+	other.swap(*this);
+	return *this;
+}
+
+
+void swap(bitmap& a, bitmap& b) noexcept
+{
+	a.swap(b);
+}
+
+void bitmap::swap(bitmap& other) noexcept
+{
+	using std::swap;
+	swap(m_interal_array, other.m_interal_array);
+	swap(m_actual_array, other.m_actual_array);
+	swap(m_rows, other.m_rows);
+	swap(m_cols, other.m_cols);
+}
+
+void bitmap::init_internal_array()
+{
+	for(size_t i = 0; i < m_rows; ++i)
+		m_interal_array[i] = m_actual_array + i * m_cols;
+}
+bool* bitmap::operator [](size_t i)
+{
+	return m_interal_array[i];
+}
+const bool* bitmap::operator [](size_t i) const
+{
+	return m_interal_array[i];
 }
 size_t bitmap::rows() const
 {
@@ -43,8 +96,14 @@ std::variant<bitmap, std::string> bitmap_factory::from_stream(size_t rows, size_
 bitmap bitmap::add_sentinels() const
 {
 	bitmap bm(m_rows + 2, m_cols + 2);
-	for(size_t i = 1; i <= m_rows; ++i)
-		for(size_t j = 1; j <= m_cols; ++j)
-			bm[i][j] = (*this)[i-1][j-1];
+	
+	for(size_t i = 0; i < m_rows + 2; ++i)
+		for(size_t j = 0; j < m_cols + 2; ++j)
+			if(i == 0 || i == m_rows + 1
+			   || j == 0 || j == m_cols + 1)
+				bm[i][j] = false;
+			else
+				bm[i][j] = (*this)[i - 1][j - 1];
 	return bm;
 }
+
--- bitmap.hpp
+++ bitmap.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <vector>
 #include <istream>
 #include <variant>
@@ -7,22 +8,35 @@ class bitmap
 {
  public:
 	bitmap(size_t rows, size_t cols);
-	std::vector<bool>& operator[](size_t i);
-	const std::vector<bool>& operator[](size_t i) const;
+	
+	bool* operator [](size_t i);
+	const bool* operator [](size_t i) const;
+	
+	bitmap(const bitmap& other);
+	bitmap(bitmap&& other) noexcept;
+	bitmap& operator =(bitmap other) noexcept;
+	~bitmap();
+	friend void swap(bitmap& a, bitmap& b) noexcept;
+	void swap(bitmap& other) noexcept;
+	
 	size_t rows() const;
 	size_t cols() const;
 	bitmap add_sentinels() const;
  private:
-	std::vector<std::vector<bool>> m_map;
+	void init_internal_array();
 	size_t m_rows, m_cols;
+	bool** m_interal_array;
+	bool* m_actual_array;
 };
+
+
 class bitmap_factory
 {
  public:
 	bitmap_factory() = delete;
 	bitmap_factory(const bitmap_factory&) = delete;
 	bitmap_factory(bitmap_factory&&) = delete;
-	static std::variant<bitmap,std::string> from_stream(size_t rows, size_t cols, std::istream& is);
+	static std::variant<bitmap, std::string> from_stream(size_t rows, size_t cols, std::istream& is);
 };
 
 
